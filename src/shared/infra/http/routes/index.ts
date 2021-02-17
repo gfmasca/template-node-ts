8957cf8d647f28edb8b51f01b4ed1960491f47b4
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2021-02-17 19:45:41 -0300

    add: added users routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
 
+// Users
+import usersRoutes from '@modules/users/infra/http/routes/users.routes';
+
 const routes = Router();
+
+routes.use(usersRoutes);
 
 export default routes;
